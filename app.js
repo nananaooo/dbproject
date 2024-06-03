69635f3b43a1ea6e89febf52d3ba03d73e91e252
saens <songsh5775@gmail.com>
2024-06-04 04:37:23 +0900

    added SQL

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = 3000;
+const port = 3123;
+const cors = require('cors');
+
+app.use(cors());
 
 // MySQL 연결 설정
 const connection = mysql.createConnection({
   host: 'localhost', // MySQL 호스트 이름
-  user: 'minjae', // MySQL 사용자 이름
-  password: 'jbnu', // MySQL 암호
-  database: 'dbname' // 연결할 MySQL 데이터베이스 이름
+  user: 'root', // MySQL 사용자 이름
+  password: 'qweasd1!', // MySQL 암호
+  database: 'cars' // 연결할 MySQL 데이터베이스 이름
 });
+
+fuel_prices = {
+  '디젤': 1500,
+  '가솔린': 1700
+};
 
 // MySQL 연결
 connection.connect((err) => {
@@ -21,33 +29,43 @@ connection.connect((err) => {
   console.log('MySQL 연결 ID: ' + connection.threadId);
 });
 
-// 루트 경로에 대한 GET 요청 처리
-app.get('/', (req, res) => {
-  // MySQL에서 데이터 조회 예제
-  connection.query('SELECT * FROM cars', (error, results, fields) => {
-    if (error) {
-      res.status(500).send('데이터 조회 실패');
-      throw error;
-    }
-    
-    // 조회 결과를 클라이언트에게 응답
-    res.send(results);
-  });
-});
 app.get('/recommend', (req, res) => {
   // 클라이언트가 보낸 파라미터 가져오기
-  const { price, month_budget, km } = req.query;
+  const { price, month_budget, km} = req.query;
 
   // 각 파라미터가 주어졌는지 확인
   if (!price || !month_budget || !km) {
       return res.status(400).json({ error: 'Missing parameters' });
   }
 
-  // 각 파라미터를 숫자로 변환하여 더하기
-  const totalValue = parseInt(price) + parseInt(month_budget) + parseInt(km);
+
+  const selectCarsQuery = `
+    SELECT company, name, price, type, fuel, cc, efficiency, tax, oil_price,
+           (oil_price * ${km} / efficiency + tax) as expense,
+    (
+      ABS((price - ${price}) / stddev_price) +
+      ABS((oil_price * ${km} / efficiency + tax - ${month_budget}) / stddev_expense)
+    ) / 2 as score
+    FROM cars
+    CROSS JOIN 
+        (SELECT 
+            STDDEV_SAMP(price) as stddev_price, 
+            STDDEV_SAMP(oil_price * ${km} / efficiency + tax) as stddev_expense
+        FROM cars) AS stats
+    ORDER BY score
+    LIMIT 10;
+  `
+
+  connection.query(selectCarsQuery, (err, results) => {
+    if (err) {
+      console.error('데이터 조회 실패:', err);
+      return;
+    }
+    console.log('조회된 데이터:', results);
+    res.json({results});
+  });
     
   // 클라이언트에게 결과 보내기
-  res.json({ totalValue });
 });
 
 // 다른 경로에 대한 GET 요청 처리
